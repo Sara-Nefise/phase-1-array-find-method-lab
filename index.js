82178753a866e39b35a106afb26dfbe47ac209e3
saranafisa <sara_nafisa@protonmail.com>
2023-04-11 12:28:42 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+let arr=[
+   { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+function superbowlWin(array){
+let obj=array.find(element => element.result=='W')
+return obj?obj.year:undefined;
+}
+
+console.log(superbowlWin(arr))
